8999406457fb5ea278032ef0aa0efaba9de33fef
Aayush Soni <soniaayush5562@gmail.com>
2024-07-26 13:14:13 +0530

    Update JwtToken.js

--- utills/JwtToken.js
+++ utills/JwtToken.js
@@ -6,7 +6,7 @@ const SendToken = async (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        httpOnly: false,
     };
 
     res.status(statusCode).cookie("token", token, options).json({

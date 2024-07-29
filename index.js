60cfb99696e73b5b71d404860043f57e2a83c71d
Aayush Soni <soniaayush5562@gmail.com>
2024-07-29 12:25:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config({ path: "./.env" });
 import cloudinary from "cloudinary";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
+    api_key: 142813777265362,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 import connectDB from "./db/DB.js";

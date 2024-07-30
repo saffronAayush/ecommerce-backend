d13c40a3dc99cd380611ac29e3d5e0f10629a841
Aayush Soni <soniaayush5562@gmail.com>
2024-07-30 11:36:24 +0530

    Update SendEmail.js

--- utills/SendEmail.js
+++ utills/SendEmail.js
@@ -3,10 +3,18 @@ import nodemailer from "nodemailer";
 const SendEmail = async ({ email, subject, message }) => {
     const transporter = nodemailer.createTransport({
         service: process.env.SMPT_SERVICE,
+        port: 465,
+        secure: true,
+        logger: true,
+        debug: true,
+        secureConnection: false,
         auth: {
             user: process.env.SMPT_MAIL,
             pass: process.env.SMPT_PASSWORD,
         },
+        tls:{
+            rejectUnAuthorized:true
+        }
     });
 
     const mailOptions = {

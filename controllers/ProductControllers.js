05e7a99d5eb51e2fe2f0371ed2fab3ac677d0a5b
Aayush Soni <soniaayush5562@gmail.com>
2024-07-30 12:53:06 +0530

    Update ProductControllers.js

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -14,8 +14,6 @@ const CreatProduct = CatchAsynError(async (req, res, next) => {
             cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
             api_key: process.env.CLOUDINARY_API_KEY,
             api_secret: process.env.CLOUDINARY_API_SECRET
-                ? "exists"
-                : "not set",
         });
 
         // Parse images from request body
